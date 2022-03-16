7477f9dcab87f241bf48b1211e50fd80999a2687
Peacebern <peacebern@gmail.com>
2022-03-16 13:20:24 -0700

    fixed

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,8 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-* Return: Always 0 (Success)
+*
+* Return: 0
 */
 
 int main(void)
@@ -11,5 +12,6 @@ int main(void)
 
         i = 0;
         positive_or_negative(i);
-        return (0);
+        
+	return (0);
 }
