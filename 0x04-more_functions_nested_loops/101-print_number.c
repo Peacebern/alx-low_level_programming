8c6364be5153a181b55d68fe3abbe1934c9086d7
Peacebern <peacebern@gmail.com>
2022-03-17 15:50:53 -0700

    fixed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,15 +8,17 @@ void print_integer(int m);
  */
 void print_number(int n)
 {
-	if (n == 0)
+	if (n == 0) {
 		_putchar('0');
+	}
 	else if (n < 0)
 	{
 		_putchar('-');
 		print_integer(n * -1);
 	}
-	else
+	else {
 		print_integer(n);
+	}
 }
 
 /**
