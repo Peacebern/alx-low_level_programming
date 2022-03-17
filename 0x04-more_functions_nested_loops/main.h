086e527ea5679500a58328309c7109fd26eedb9a
Peacebern <peacebern@gmail.com>
2022-03-17 16:46:45 -0700

    fixed

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,13 +1,11 @@
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month,int day);
-void print_remaining_days(int month,int day,int year);
-void print_to_98(int n);
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
@@ -15,5 +13,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-void print_integer(int m);
-void print_integer(int m);
+
+#endif
