c784204a1aa36344cd175daa472286c417da01e4
Peacebern <peacebern@gmail.com>
2022-04-07 12:42:25 -0700

    trying

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -9,9 +10,11 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-void errors(void);
-int _strlen(char *s);
-int is_digit(char *s);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _strlen(char *);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int onlyNumbers(char *c);
+void multiply(char* s1, char* s2);
 
 #endif
