c0387b90f42e4ffe58211e5ba528eaef35b3c839
Peacebern <peacebern@gmail.com>
2022-04-07 19:17:40 -0700

    fixed

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,40 +11,31 @@
 int main(int argc, char **argv)
 {
 	int num1, num2, mul;
-	char cn[6];
-	char ch = 'E';
+	char cn[10];
+	char ch[7] = "Error\n";
 	int i;
+	int r;
 
 	if (argc != 3)
 	{
-		_putchar(ch);
-		
+		for (r = 0; r < 7; r++)
+		{
+		_putchar(ch[r]);
+		}
 		exit(98);
 	}
 	else
 	{
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
-
-		printf("%d\n", num1);
-		printf("%d\n", num2);
-
-
 		mul = num1 * num2;
-		
 		sprintf(cn, "%d", mul);
-	
 		for (i = 0; i < 3; i++)
 		{
-
-		
-
 		_putchar(cn[i]);
 		}
-
-
+		_putchar('\n');
 
 		return (0);
 	}
 }
-
