bccc7d8132816d4729787cb14468b70146c0c2be
Peacebern <peacebern@gmail.com>
2022-04-14 06:29:36 -0700

    sum of all unsigned int, regardless of the number of parameters the function allows, this is possible due to my use of variadi function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * sum_them_all - sums of all its parameters.
+ * @n: number of parameters
+ *
+ * Return: sum of all parameters.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap; /* 'ap' here stands for arguement parameter */
+
+	va_start(ap, n);
+	unsigned int i, sum;
+	i = 0;
+	sum = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, unsigned int);
+	}
+	va_end(ap);
+	return sum;
+}
